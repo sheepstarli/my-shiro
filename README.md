ed0e0f7b33893502af1bb76649b8a10bab4c5275
李晨星 <lichenxing@ruyicai.com>
2013-10-30 10:21:44 +0800

    change something

--- README.md
+++
@@ -1,4 +0,0 @@
-my-shiro
-========
-
-shiro cas
--- src/main/java/com/ruyicai/shiro/controller/TestController.java
+++ src/main/java/com/ruyicai/shiro/controller/TestController.java
@@ -9,14 +9,15 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/test")
 @Controller
 public class TestController {
-
+	
 	private Logger logger = LoggerFactory.getLogger(TestController.class);
 	
 	@RequestMapping("/index")
 	public ModelAndView index(ModelAndView mav) {
 		logger.info("/test/index");
-		mav.addObject("message", "这是测试！！！");
-		mav.setViewName("/showMessage");
+		mav.addObject("message", "这里是主页");
+		mav.setViewName("showMessage");
 		return mav;
 	}
+
 }
