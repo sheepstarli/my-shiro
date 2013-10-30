678a57a0ed5b4912955bd83e591b6fc7efd5b740
李晨星 <lichenxing@ruyicai.com>
2013-10-30 09:09:27 +0800

    commit test

---
+++ src/test/java/com/ruyicai/shiro/ShiroTest.java
@@ -0,0 +1,14 @@
+package com.ruyicai.shiro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShiroTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
